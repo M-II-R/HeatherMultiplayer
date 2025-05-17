11521db179bcab9d25792412e5dc4c3b2b6c43d7
M-II-R <miroslavik.polev@gmail.com>
2025-05-17 18:02:44 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -830,6 +830,7 @@ wss.on("connection", (client) => {
                                             clfmess.push(clie);
                                         });*/
                                         //setTimeout(() => {
+                                        console.log(game.clients.length);
                                         for (let i = 0; i < game.clients.length; i++) {
                                             let cl = game.clients[i];
                                             cl.socket.send(JSON.stringify(Message.Create({ "id": cmess.id, "players": clfmess, "plnumb": rcl[cmess.gametype].plnumb, "plinteam": rcl[cmess.gametype].plinteam, /*"team":*/ "data": "", "gid": game.GameID }, "GameStart")));
